622e9a503b281ad2836bfa21cd2a40ac8556852a
Sky Yin <sky.yin@gmail.com>
2023-05-23 10:29:20 -0700

    staged change

---
+++ prompt_template.py
@@ -0,0 +1,13 @@
+from langchain import PromptTemplate
+
+template = """Question: {question}
+
+Answer: """
+
+prompt = PromptTemplate(
+        template=template,
+    input_variables=['question']
+)
+
+# user question
+question = "Which NFL team won the Super Bowl in the 2010 season?"
