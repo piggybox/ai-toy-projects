daba14c754bdee0fb6f974adc2b64c486c5a3c07
piggybox <sky.yin@gmail.com>
2024-05-01 00:50:17 -0700

    Construct style prompt

--- openai-writer/app/service/api.ts
+++ openai-writer/app/service/api.ts
@@ -8,10 +8,27 @@ const openAIChatWrapper = async (promptValue: string) => {
         model: "gpt-3.5-turbo",
         messages: [
             { "role": "assistant", "content": "You are a helpful assistant." },
-            { role: "user", content: promptValue }],
+            { "role": "user", "content": promptValue }],
     });
 
     return completion.choices[0].message.content;
 };
 
-export default openAIChatWrapper
+export const getStyledText = async (content: string, context: string) => {
+    let promptValue = `Below content is part of a writing article.`
+    switch (context) {
+        case "summarize":
+            promptValue += `Summarize the below content. \n "${content}"`;
+            break;
+        case "vocab":
+            promptValue += `Provide some good vocabulary suggestions for the below content as a list of words with their mapping from existing words in article. \n "${content}"`;
+            break;
+        case "improve":
+            promptValue += `Improve the below content by making it even better. \n "${content}"`;
+            break;
+        default:
+            return content;
+    }
+    const styledContent = openAIChatWrapper(promptValue);
+    return styledContent;
+}
