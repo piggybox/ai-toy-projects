442ec98f4c0ecb1e62e92c957291193be197cac5
piggybox <sky.yin@gmail.com>
2024-05-04 23:13:48 -0700

    Improve type setting

--- openai-writer/app/service/api-wrapper.ts
+++ openai-writer/app/service/api-wrapper.ts
@@ -1,11 +1,17 @@
 import { getStyledText } from "./api-requester"
 import { getToneSuggestion } from "./api-requester"
 
-export const getResult = async ({ content, type, context }) => {
+interface input {
+    content: string
+    type: string 
+    context: string
+}
+
+export const getResult = async ({ content, type, context }: input) => {
     switch (type) {
         case "style":
             return getStyledText(content, context);
-        case "vocab":
+        case "tone":
             return getToneSuggestion(content, context);
         default:
             return "Invalid type";
