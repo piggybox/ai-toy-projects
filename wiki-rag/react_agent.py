b587402a281446a5e014cee57f986431d89bd902
piggybox <sky.yin@gmail.com>
2024-05-05 12:46:00 -0700

    Setup for Wiki RAG project

---
+++ wiki-rag/react_agent.py
@@ -0,0 +1,1 @@
+# ReAct style AI agent
---
+++ wiki-rag/utils.py
@@ -0,0 +1,24 @@
+import os 
+import yaml 
+
+def get_apikey():
+    """
+    Reads API key from a configuration file.
+
+    This function opens a configuration file named "apikeys.yml", reads the API key for OpenAI
+
+    Returns:
+    api_key (str): The OpenAI API key.
+    """
+    
+    # Construct the full path to the configuration file
+    script_dir = "."
+    file_path = os.path.join(script_dir, "apikeys.yml")
+
+    with open(file_path, 'r') as yamlfile:
+        API_KEY = yaml.safe_load(yamlfile)['openai']['api_key']
+        
+    return API_KEY
+
+if __name__ == "__main__":
+    print("API_KEY", get_apikey())
