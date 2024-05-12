fdae13aee78fb31dcd28851806830cbbfd77fb3a
piggybox <sky.yin@gmail.com>
2024-05-12 14:05:18 -0700

    Index wiki

---
+++ wiki-rag/wiki_indexer.py
@@ -0,0 +1,52 @@
+from llama_index import download_loader, VectorStoreIndex, ServiceContext
+from llama_index.node_parser import SimpleNodeParser
+from llama_index.text_splitter import get_default_text_splitter
+import openai
+from pydantic import BaseModel
+from llama_index.program import OpenAIPydanticProgram
+from utils import get_apikey
+
+
+# define the data model in pydantic
+class WikiPageList(BaseModel):
+    "Data model for WikiPageList"
+    pages: list
+
+
+def wikipage_list(query):
+    openai.api_key = get_apikey()
+
+    prompt_template_str = """
+    Given the input {query}, 
+    extract the Wikipedia pages mentioned after 
+    "please index:" and return them as a list.
+    If only one page is mentioned, return a single
+    element list.
+    """
+    program = OpenAIPydanticProgram.from_defaults(
+        output_cls=WikiPageList,
+        prompt_template_str=prompt_template_str,
+        verbose=True,
+    )
+
+    wikipage_requests = program(query=query)
+
+    return wikipage_requests
+
+
+def create_wikidocs(wikipage_requests):
+    
+    return documents
+
+
+def create_index(query):
+    global index
+
+
+    return index
+
+
+if __name__ == "__main__":
+    query = "/get wikipages: paris, lagos, lao"
+    index = create_index(query)
+    print("INDEX CREATED", index)
