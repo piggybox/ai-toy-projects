5bb354f5d3e605913fc3b9e57d40e8989aec3a6f
piggybox <sky.yin@gmail.com>
2024-05-12 14:28:16 -0700

    Create docs

--- wiki-rag/wiki_indexer.py
+++ wiki-rag/wiki_indexer.py
@@ -1,9 +1,9 @@
-from llama_index import download_loader, VectorStoreIndex, ServiceContext
-from llama_index.node_parser import SimpleNodeParser
-from llama_index.text_splitter import get_default_text_splitter
+from llama_index.core import download_loader, VectorStoreIndex, ServiceContext
+from llama_index.core.node_parser import SimpleNodeParser
+# from llama_index.text_splitter import get_default_text_splitter
 import openai
 from pydantic import BaseModel
-from llama_index.program import OpenAIPydanticProgram
+from llama_index.program.openai import OpenAIPydanticProgram
 from utils import get_apikey
 
 
@@ -35,7 +35,9 @@ def wikipage_list(query):
 
 
 def create_wikidocs(wikipage_requests):
-    
+    wikipediaReader = download_loader("WikipediaReader")()
+    documents = wikipediaReader.load_data(pages=wikipage_requests)
+
     return documents
 
 
@@ -47,6 +49,8 @@ def create_index(query):
 
 
 if __name__ == "__main__":
-    query = "/get wikipages: paris, lagos, lao"
-    index = create_index(query)
-    print("INDEX CREATED", index)
+    # query = "/get wikipages: paris, lagos, lao"
+    # index = create_index(query)
+    # print("INDEX CREATED", index)
+
+    print(create_wikidocs("paris"))
